bb0f038113c9d17fb5d387f0cf1f3cf3f982c4f2
polterguy <polterguy@66c88984-4ccd-11de-9ee4-913ee803a954>
2008-09-08 16:30:52 +0000

    Fixed so that only one BehaviorDraggable fires when multiple exists on same form

--- trunk/Samples/Behaviors.aspx.cs
+++ trunk/Samples/Behaviors.aspx.cs
@@ -14,5 +14,14 @@ public partial class Behaviors : System.Web.UI.Page
     protected void dragger_Dropped(object sender, EventArgs e)
     {
 		lbl.Text = string.Format("Label dragged and dropped to {0}, {1}", lbl.Style["left"], lbl.Style["top"]);
+		Effect effect = new EffectFadeIn(lbl, 0.4M);
+		effect.Render();
+    }
+
+    protected void dragger2_Dropped(object sender, EventArgs e)
+    {
+		lbl2.Text = string.Format("Label dragged and dropped to {0}, {1}", lbl2.Style["left"], lbl2.Style["top"]);
+		Effect effect = new EffectFadeIn(lbl2, 0.4M);
+		effect.Render();
     }
 }
