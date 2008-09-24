522669c29429c0cd0e0483cab5948595d45ad1a2
kariem.elkoush <kariem.elkoush@66c88984-4ccd-11de-9ee4-913ee803a954>
2008-09-24 22:15:03 +0000

    Refactored Ra.Build.Tasks and created an abstract base class for minification (MinificationTask), it includes all logic except the minification logic itself, which is implemented in CssMinTask and JsMinTask accordingly.

--- Ra.Build.Tasks/CssMinTask.cs
+++ Ra.Build.Tasks/CssMinTask.cs
@@ -1,15 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
-using NAnt.Core;
-using NAnt.Core.Attributes;
-using NAnt.Core.Types;
-using System.Xml;
-using System.Globalization;
-using NAnt.Core.Tasks;
-using System.Text.RegularExpressions;
-
 /* 
 Copyright 2008 - Thomas Hansen thomas@ra-ajax.org
 
@@ -34,107 +22,18 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using NAnt.Core.Attributes;
+
 namespace Ra.Build.Tasks
 {
     [TaskName("cssmin")]
-    public class CssMinTask : JsMinTask
+    public class CssMinTask : MinificationTask
     {
-        #region Private Fields
-
-        private bool _inPlace;
-
-        #endregion
-
-        #region Public Properties
-
-        [TaskAttribute("inplace")]
-        [BooleanValidator()]
-        public virtual bool InPlace
-        {
-            get { return _inPlace; }
-            set { _inPlace = value; }
-        }
-
-        [BuildElement("fileset", Required = true)]
-        public virtual FileSet CssFiles
-        {
-            get { return base.JsFiles; }
-            set { base.JsFiles = value; }
-        }
-
-        [TaskAttribute("todir")]
-        public override DirectoryInfo ToDirectory
-        {
-            get { return base.ToDirectory; }
-            set { base.ToDirectory = value; }
-        }
-
-        #endregion
-
-        protected override void InitializeTask(XmlNode taskNode)
-        {
-            if (!_inPlace && ToDirectory == null)
-                throw new BuildException(
-                    string.Format(
-                        CultureInfo.InvariantCulture, 
-                        "The 'todir' attribute must be set to specify the output directory of the minified CSS files."),
-                    Location);
-
-            if (CssFiles == null)
-                throw new BuildException(
-                    string.Format(
-                        CultureInfo.InvariantCulture,
-                        "The <fileset> element must be used to specify the CSS files to minify."), 
-                    Location);
-
-            if (ToDirectory != null && !_inPlace && !ToDirectory.Exists)
-                ToDirectory.Create();
-        }
-        
-        protected override void ExecuteTask()
-        {
-            if (CssFiles.BaseDirectory == null)
-                CssFiles.BaseDirectory = new DirectoryInfo(Project.BaseDirectory);
-
-            Log(Level.Info, "Minifying {0} CSS file(s).", CssFiles.FileNames.Count);
-
-            foreach (string srcPath in CssFiles.FileNames)
-            {
-                FileInfo srcFile = new FileInfo(srcPath);
-
-                if (srcFile.Exists)
-                {
-                    string destPath;
-
-                    if (!_inPlace)
-                    {
-                        destPath = GetDestPath(CssFiles.BaseDirectory, srcFile);
-                        DirectoryInfo destDir = new DirectoryInfo(Path.GetDirectoryName(destPath));
-
-                        if (!destDir.Exists)
-                            destDir.Create();
-                    }
-                    else
-                    {
-                        destPath = srcPath;
-                    }
-                   
-                    Log(Level.Verbose, "Minifying '{0}' to '{1}'.", srcPath, destPath);
-                    MinifyCss(srcPath, destPath);
-                }
-                else
-                {
-                    throw new BuildException(
-                        string.Format(
-                            CultureInfo.InvariantCulture,
-                            "Could not find file '{0}' to minify.",
-                            srcFile.FullName),
-                        Location);
-                }
-            }
-        }
-
-        protected void MinifyCss(string srcPath, string destPath)
+        protected override void Minify(string srcPath, string destPath)
         {
             string css;
             using (StreamReader sr = new StreamReader(srcPath))
@@ -160,7 +59,7 @@ namespace Ra.Build.Tasks
             newCss = Regex.Replace(newCss, "\\s*;\\s*", ";", RegexOptions.Compiled | RegexOptions.ECMAScript);
 
             using (StreamWriter sw = new StreamWriter(destPath))
-                sw.WriteLine(newCss);
+                sw.Write(newCss);
         }
     }
 }
