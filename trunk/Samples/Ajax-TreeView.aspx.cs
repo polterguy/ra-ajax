a5632351a508744c8d492ec9d70ebee92b71e114
polterguy <polterguy@66c88984-4ccd-11de-9ee4-913ee803a954>
2008-10-03 21:50:25 +0000

    TreeView implementation....

--- trunk/Samples/Ajax-TreeView.aspx.cs
+++ trunk/Samples/Ajax-TreeView.aspx.cs
@@ -22,21 +22,45 @@ namespace Samples
 
             // First child TreeViewItem
             TreeViewItem item = new TreeViewItem();
-            item.ID = "HTML";
+            item.ID = "HTML_normal_tree";
             ASPCTRLS.LiteralControl lit = new ASPCTRLS.LiteralControl();
             lit.Text = "HTML";
-            lit.ID = "HTML_normal";
+            lit.ID = "HTML_normal_lit";
             item.Controls.Add(lit);
             parent.AddTreeViewItem(item);
 
             // Second child TreeViewItem
             item = new TreeViewItem();
-            item.ID = "XHTML";
+            item.ID = "XHTML_tree";
             lit = new ASPCTRLS.LiteralControl();
             lit.Text = "XHTML";
-            lit.ID = "XHTML";
+            lit.ID = "XHTML_lit";
             item.Controls.Add(lit);
             parent.AddTreeViewItem(item);
+        }
+
+        protected void selected(object sender, EventArgs e)
+        {
+            TreeViewItem item = sender as TreeViewItem;
+            pnlOutput1.Text = item.ID + " was selected";
+            pnlOutput2.Text = GetTextForSelection(item.ID);
+            Effect effect = new EffectHighlight(pnl, 500);
+            effect.ChainThese(
+                new EffectHighlight(pnlOutput1, 500),
+                new EffectHighlight(pnlOutput2, 500)
+                );
+            effect.Render();
+        }
+
+        private string GetTextForSelection(string id)
+        {
+            switch (id)
+            {
+                case "good":
+                    return "These are the really good things for developers in todays world";
+                default:
+                    return "I don't really have any intelligent to say about that theme...";
+            }
         }
     }
 }
