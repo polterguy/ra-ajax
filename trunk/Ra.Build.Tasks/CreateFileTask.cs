e878a030cdbe9fbd2f9cb94607764768a976e2eb
kariem.elkoush <kariem.elkoush@66c88984-4ccd-11de-9ee4-913ee803a954>
2009-06-23 14:39:38 +0000

    Modified the build system to use the C# 3.0 Compiler, still targeting .NET Framework 2.0

--- trunk/Ra.Build.Tasks/CreateFileTask.cs
+++ trunk/Ra.Build.Tasks/CreateFileTask.cs
@@ -27,13 +27,12 @@ Kariem Ali (kariem@ra-ajax.org)
 
 */
 
-using System;
+using System.Globalization;
 using System.IO;
 using System.Xml;
-using System.Globalization;
 using NAnt.Core;
-using NAnt.Core.Types;
 using NAnt.Core.Attributes;
+using NAnt.Core.Types;
 
 namespace Ra.Build.Tasks
 {
